a8cc3cdbbb7f5848f11cf7bee17fc96f8942ab59
rjha18 <rjha18@live.com>
2020-03-03 17:38:55 -0800

    Working route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hi' });
+  res.render('index', { title: "Wait!", url: req.query.url, sec: 5 });
 });
 
 module.exports = router;
